64235e5a9a63535ed813787afc9e8a516991af0a
captainissolozy <56184921+captainissolozy@users.noreply.github.com>
2022-02-26 12:38:50 +0700

    Before add styled components

---
+++ src/components/navbar/index.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand justify-content-center text-white" href="#">Logo</a>
+          <a className="navbar-brand text-white" href="#">Push, The XO</a>
+          <a className="navbar-brand text-white" href="#">About</a>
+        </div>
+      </nav>
+    )
+}
+export default Navbar
---
+++ src/pages/home/index.js
@@ -0,0 +1,48 @@
+const Home = () => {
+    return (
+        <div className='box'>
+        <div className='container my-3 justify-content-center d-flex c-box'>
+          
+            <div className='row my-3 border border-primary w-100'>
+              <div className='col'>
+                <div className='col align-self-center py-3'>
+
+                  <div className='row m-3'>
+
+                    <div className='col align-self-center m-3'>
+
+                      <button type="button" className="btn btn-dark home-btn">Log-in</button>
+
+                    </div>
+
+                  </div>
+                  <div className='row m-3'>
+
+                    <div className='col align-self-center m-3'>
+
+                      <button type="button" className="btn btn-dark home-btn">Sign-in</button>
+
+                    </div>
+
+                  </div>
+                  <div className='row m-3'>
+
+                    <div className='col align-self-center m-3'>
+
+                      <button type="button" className="btn btn-dark home-btn">Guest</button>
+
+                    </div>
+
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+          
+        </div>
+      </div>
+    )
+} 
+export default Home
